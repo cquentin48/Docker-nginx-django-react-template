ac67633fee6a5c783bfb5ae104e1e670f7cbae4e
Quentin CHAPEL <chapelquen@gmail.com>
2023-02-28 10:36:13 +0100

    [Feat|WIP|Init]First commit

--- backend/data/urls.py
+++ backend/data/urls.py
@@ -3,6 +3,9 @@ from django.views.decorators.csrf import csrf_exempt
 
 from graphene_django.views import GraphQLView
 
+from .views import index
+
 urlpatterns=[
     re_path("graphql/", csrf_exempt(GraphQLView.as_view(graphiql=True))),
+    re_path('hello-world/', index,name="index")
 ]
